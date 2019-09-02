919278f2c397278e3299a17c7844824293b0e0c9
ryleybridges <rye.brid@gmail.com>
2019-09-03 11:23:27 +1200

    Set up config and server connection

--- server.js
+++ server.js
@@ -5,3 +5,28 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+
+const config = require('./config');
+
+mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@ryleyscluster-jy7ku.mongodb.net/portfolio?retryWrites=true&w=majority`);
+
+const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    // we're connected!
+    console.log('we are connected to mongo db');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+app.use(function(req, res, next){
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
+
+app.listen(port, () => {
+  console.clear();
+  console.log(`application is running on port ${port}`)
+});
